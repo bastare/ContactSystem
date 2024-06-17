8edd68eb51c17cf83cc96c2b13feb64b74da8ad0
reds <yaroslav.fesiuk@tape-cat.com>
2024-06-17 03:53:30 -0400

    Refactoring && add `projection` on query

--- src/client/src/app/contact-container/components/contact-table/contact-table.component.ts
+++ src/client/src/app/contact-container/components/contact-table/contact-table.component.ts
@@ -1,18 +1,13 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatTableDataSource } from '@angular/material/table';
-import { ContactState } from '../../store-features/contact-state.model';
-import { Store, select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import {
   AppState,
-  selectAll,
-  selectContactsForTable,
 } from '../../store-features/contact.reducer';
 import { AddFormComponent } from './atoms/add-form/add-form.component';
 import { ContactRestActions } from '../../store-features/actions/contact-rest.actions';
 import { TableComponent } from './atoms/table/table.component';
-import { map, tap } from 'rxjs';
 import { TablePaginatorComponent } from './atoms/table-paginator/table-paginator.component';
 import { TableFilterComponent } from './atoms/table-filter/table-filter.component';
 import { MatButton } from '@angular/material/button';
@@ -49,6 +44,7 @@ export class ContactTableComponent implements OnInit {
             && !string.IsNullOrEmpty(LastName)
             && !string.IsNullOrEmpty(Email)
             && !string.IsNullOrEmpty(Phone)`,
+        projection: 'new(Id, FirstName, LastName, Title, Email, Phone, MiddleInitial)',
         offset: 1,
         limit: 10,
       })
