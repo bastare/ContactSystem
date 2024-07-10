4223e2391dcccc0ff0b13bdbba8317633adf034c
Yaroslav Fesiuk <yaroslav@gopineapple.com>
2024-07-10 02:15:04 -0400

    Refactoring

--- src/server/ContactSystem.Core/Api/Filters/ValidationFilter.cs
+++ src/server/ContactSystem.Core/Api/Filters/ValidationFilter.cs
@@ -8,32 +8,37 @@ public sealed class ValidationFilter : IEndpointFilter
 {
 	public async ValueTask<object?> InvokeAsync ( EndpointFilterInvocationContext context , EndpointFilterDelegate next )
 	{
-		var validationErrors =
-			context.Arguments
-				.Where ( entity => entity is IHasValidation )
-				.Cast<IHasValidation> ()
-				.Aggregate (
-					seed: new Stack<IEnumerable<string>> () ,
-					( errorMessages , entityForValidation ) =>
-					{
-						var validationResult = entityForValidation.Validate ();
+		if ( TryResolveValidationErrors ( out var validationErrors_ , context.Arguments ) )
+			return Results.BadRequest (
+				error: new ErrorMessage (
+					Message: string.Join (
+						separator: ' ' ,
+						values: validationErrors_ ) ) );
 
-						if ( validationResult.IsValid )
+		return await next ( context );
+
+		static bool TryResolveValidationErrors ( out ImmutableList<string> validationErrors_ , IList<object?> arguments )
+		{
+			validationErrors_ =
+				arguments
+					.Where ( entity => entity is IHasValidation )
+					.Cast<IHasValidation> ()
+					.Aggregate (
+						seed: new Stack<IEnumerable<string>> () ,
+						( errorMessages , entityForValidation ) =>
+						{
+							var validationResult = entityForValidation.Validate ();
+
+							if ( validationResult.IsValid )
+								return errorMessages;
+
+							errorMessages.Push ( validationResult.Errors.Select ( error => error.ErrorMessage ) );
 							return errorMessages;
+						} )
+					.SelectMany ( errorMessages => errorMessages )
+					.ToImmutableList ();
 
-						errorMessages.Push ( validationResult.Errors.Select ( error => error.ErrorMessage ) );
-						return errorMessages;
-					} )
-				.SelectMany ( errorMessages => errorMessages )
-				.ToImmutableList ();
-
-		if ( validationErrors is { Count: <= 0 } )
-			return await next ( context );
-
-		return Results.BadRequest (
-			error: new ErrorMessage (
-				Message: string.Join (
-					separator: ' ' ,
-					values: validationErrors ) ) );
+			return validationErrors_ is { Count: > 0 };
+		}
 	}
 }
